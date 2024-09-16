4b9785123549257f4e2eb64d1a81151ae665fee3
Rama116 <ramagugan77@gmail.com>
2024-09-16 15:39:15 +0530

    Updated Login

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ const App = () => {
     <>
     <div className="flex h-screen w-screen justify-center items-center bg-green-200">
 
-       <div className="h-4/6 w-[33%] flex justify-center items-center bg-slate-100 relative">
-         <h1 className="absolute top-[10%] text-black text-2xl font-bold">Login Form</h1>
+       <div className="h-4/6 w-[30%] flex justify-center items-center bg-green-100 relative">
+         <h1 className="absolute top-[5%] text-black text-2xl font-bold underline">Login Form</h1>
               
           <form className="h-[70%] w-[70%]  flex flex-col justify-center items-center gap-7">
             <input type="text" name="txt" id="txt" placeholder="Name" className="p-3 w-full outline-none focus:border-b-4 hover:border-green-400"/>
             <input type="email" name="email" id="email" placeholder="abc123@gmail.com" className="p-3 w-full outline-none focus:border-b-4 hover:border-green-400"/>
             <input type="password" name="pass" id="pass" placeholder="Password" className="p-3 w-full outline-none focus:border-b-4 hover:border-green-400"/>
-            <button type="submit" className="p-2 text-white bg-green-400 w-[80%] font-bold outline-none focus:border-b-4">submit</button>
+            <a href="" target="blank" className="font-medium flex text-base pl-48 outline:none hover:underline text-blue-500"><p>Forgot Password?</p></a>
+            <button type="submit" className="p-3 text-white bg-green-400 w-[75%] font-bold outline-none hover:bg-green-500">submit</button>
           </form>
         </div>
     </div>
@@ -20,6 +21,6 @@ const App = () => {
         
     </>
   )
-}
+} 
 
 export default App;
