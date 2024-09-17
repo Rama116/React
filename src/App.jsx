068d37b1b40552e83f52c337c8d68e6f6c76a9a0
Rama116 <ramagugan77@gmail.com>
2024-09-17 08:57:53 +0530

    tried navbar myself

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 // import React from 'react'
+import NavBar from "./components/NavBar";
 
 const App = () => {
+
   return (
     <>
-    <div className="flex h-screen w-screen justify-center items-center bg-green-200">
-
-       <div className="h-4/6 w-[30%] flex justify-center items-center bg-green-100 relative">
-         <h1 className="absolute top-[5%] text-black text-2xl font-bold underline">Login Form</h1>
-              
-          <form className="h-[70%] w-[70%]  flex flex-col justify-center items-center gap-7">
-            <input type="text" name="txt" id="txt" placeholder="Name" className="p-3 w-full outline-none focus:border-b-4 hover:border-green-400"/>
-            <input type="email" name="email" id="email" placeholder="abc123@gmail.com" className="p-3 w-full outline-none focus:border-b-4 hover:border-green-400"/>
-            <input type="password" name="pass" id="pass" placeholder="Password" className="p-3 w-full outline-none focus:border-b-4 hover:border-green-400"/>
-            <a href="" target="blank" className="font-medium flex text-base pl-48 outline:none hover:underline text-blue-500"><p>Forgot Password?</p></a>
-            <button type="submit" className="p-3 text-white bg-green-400 w-[75%] font-bold outline-none hover:bg-green-500">submit</button>
-          </form>
-        </div>
-    </div>
-
+      <div className="w-full min-h-screen flex flex-col fixed ">
+        {/* <NavBar appname={[1,2,3,"rama"]} /> */}
+        <NavBar />
+      </div>
         
+      <div className="flex h-screen w-screen justify-center items-center bg-green-200 overflow-hidden">
+
+      <div className="h-4/6 w-[30%] flex justify-center items-center bg-green-100 relative">
+        <h1 className="absolute top-[5%] text-black text-2xl font-bold">Login Form</h1>
+            
+        <form className="h-[70%] w-[70%]  flex flex-col justify-center items-center gap-7">
+          <input type="text" name="txt" id="txt" placeholder="Name" className="p-3 w-full outline-none focus:border-b-4 hover:border-green-400"/>
+          <input type="email" name="email" id="email" placeholder="abc123@gmail.com" className="p-3 w-full outline-none focus:border-b-4 hover:border-green-400"/>
+          <input type="password" name="pass" id="pass" placeholder="Password" className="p-3 w-full outline-none focus:border-b-4 hover:border-green-400"/>
+          <a href="" target="blank" className="font-medium flex text-base pl-48 outline:none hover:underline text-blue-500"><p>Forgot Password?</p></a>
+          <button type="submit" className="p-3 text-white bg-green-400 w-[75%] font-bold outline-none hover:bg-green-500">submit</button>
+        </form>
+      </div>
+      </div>
     </>
   )
 } 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,21 @@
-const Navbar = () => {
-        
+
+const NavBar = ({appname}) => {
+    return(
+        <>
+                <div className="w-full h-[3.5rem] grad-blue-bg text-black bg-green-400 flex justify-center">
+                  {/* <h1> {appname}</h1>  */}
+                  <div className="h-[100%] w-96 bg-slate-50">
+                    <ul className="flex flex-row gap-12 text-black font-bold text-lg pt-3 justify-center">
+                      <li >Home</li>
+                      <li>Login</li>
+                      <li>Cart</li>
+                      <li>Contact</li>
+                    </ul>
+                  </div>
+                </div>
+         </>
+    )
+
 }
+
+export default NavBar;
