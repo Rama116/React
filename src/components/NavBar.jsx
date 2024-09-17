4ac0f25f454fdb18fbef66c1deab2b26e18ff095
Rama116 <ramagugan77@gmail.com>
2024-09-17 14:14:06 +0530

    portfolio

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,15 @@
 
-const NavBar = ({appname}) => {
+const NavBar = () => {
     return(
         <>
-                <div className="w-full h-[3.5rem] grad-blue-bg text-black bg-green-400 flex justify-center">
-                  {/* <h1> {appname}</h1>  */}
-                  <div className="h-[100%] w-96 bg-slate-50">
-                    <ul className="flex flex-row gap-12 text-black font-bold text-lg pt-3 justify-center">
-                      <li >Home</li>
-                      <li>Login</li>
-                      <li>Cart</li>
+                <div className="w-full h-[4.3rem] grad-blue-bg text-black bg-purple-400 flex relative">
+                  <h1 className="text-3xl text-black absolute left-[6%] top-[20%]">Ramalingam M</h1>
+                  <ul className="flex flex-row  gap-10 text-xl absolute right-[10%] top-[32%]">
+                      <li>Profile</li>
+                      <li>Project</li>
                       <li>Contact</li>
-                    </ul>
-                  </div>
+                  </ul>  
+
                 </div>
          </>
     )
